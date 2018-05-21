2861c307e9d2a3b5e625cf31f23a09287f8e66cc
Ben <belleroseb@hotmail.com>
2018-05-21 14:52:03 -0400

    Code Style
    Updated code style

--- RaspberryPi/Control/run_mode.py
+++ RaspberryPi/Control/run_mode.py
@@ -1,50 +1,74 @@
-#*********************************************************************
-#Run Mode V_01
-#Authors:Jake Smiley & Ben Bellerose & Steven Kalapos
-#Description: This is where all outputs to devices are handled
-#*********************************************************************
+"""
+ * @file run_mode.py
+ * @authors Steven Kalapos & Ben Bellerose
+ * @date May 2018
+ * @modified May 21 2018
+ * @modifiedby Ben Bellerose
+ * @brief contains various output controls for device
+ */
+ """
 import serial
 import RPi.GPIO as GPIO
 import time
 from time import gmtime,strftime
 
-#Communication with arduino displaying our sensors
-def read_sensor():
+"""Input: no input needed for function
+   Function: reads sensor values over serial communication
+   Output: writes out array of values for all sensors or NA if there is a problem"""
+def read_Sensor():
     try:
-        ser = serial.Serial('/dev/ttyACM0', 9600)
-        hold1 = ser.readline().replace("\r", "")
-        hold1 = hold1.replace("\n", "")
-        hold1 = hold1.split("-")
-        bank = []
-        x = 0
-        while x < len(hold1):
-            hold = hold1[x].split("=")
-            bank.insert(len(bank), hold)
-            x = x + 1
+        if serial.Serial('/dev/ttyACM0', 9600):
+            ser = serial.Serial('/dev/ttyACM0', 9600)
+            hold1 = ser.readline().replace("\r", "")
+            hold1 = hold1.replace("\n", "")
+            hold1 = hold1.split("-")
+            bank = []
+            x = 0
+            while x < len(hold1):
+                hold = hold1[x].split("=")
+                bank.insert(len(bank), hold)
+                x = x + 1
+            return bank
+        else:
+            bank = ["NA"]
+            return bank
     except Exception as e:
-        print(e)
+        #print(e)
         bank = ["NA"]
-    #print(hold1)
-    return bank
+        return bank
 
-#Find the sensor value by reading ardunio
-def sensor_value(Sensor,Unit):
+"""Input: sensor - string containing the sensor you are trying to find
+          unit - string containing the unit of the sensor you are looking for
+   Function: finds your chosen sensor value from the sensor array
+   Output: writes float value for the desired sensor or NA if there is a problem"""
+def sensor_Value(sensor,unit):
     try:
-        x = 0
         values = read_sensor()
+        x = 0
 	    #print(values)
         while x < len(values):
-            if values[x][0] == Sensor:
-                sens_val = values[x][1].replace(Unit, "")
+            if str(values[x][0]) == str(sensor):
+                sens_val = values[x][1].replace(str(unit), "")
                 sens_val = float(sens_val)
                 x = len(values)
+            else:
+                sens_val = "NA"
             x = x + 1
         return sens_val
     except Exception as e:
         #print(e)
         sens_val = "ERROR"
+        return sens_val
 
-#PID controler that returns value of 0%-100%
+"""Input: SP - integer containing the setpoint value
+          PV - integer containing the present value
+          Kp - integer containing the propotional weight value
+          Ki - integer containing the integral weight value
+          Kd - integer containing the derivetive weight value
+          I - integer containing the integral value
+          E - integer containing the error value
+   Function: this is a PID control loop useful for fine tuning controls
+   Output: writes out integer value between 0-100"""
 def PID(SP,PV,Kp,Ki,Kd,I,E):
     E2 = E
     E = SP - PV
@@ -57,81 +81,100 @@ def PID(SP,PV,Kp,Ki,Kd,I,E):
         Output = 100
     return Output,E
 
-#Setup for raspberrypi gpio output
-def InitalizeOut(Pin):
+"""Input: pin - integer value containing the desired pin
+   Function: set a desired pin to an output
+   Output: returns a boolean to inform user when done"""
+def initalizeOut(pin):
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(Pin, GPIO.OUT)
-    GPIO.output(Pin, GPIO.HIGH) #Initalize as off
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH) #Initalize as off
     time.sleep(0.5)
     return True
 
-#Setup for raspberrypi gpio input
-def InitalizeIn(Pin):
+"""Input: pin - integer value containing the desired pin
+   Function: set a desired pin to an input
+   Output: returns a boolean to inform user when done"""
+def initalizeIn(pin):
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(Pin, GPIO.IN)
+    GPIO.setup(pin, GPIO.IN)
     time.sleep(0.5)
     return True
 
-#Power control for light
-def Light(Pin, Light):
-    InitalizeOut(Pin)
+"""Input: pin - integer value containing the light pin location
+          light - float value containing the desired output time of light
+   Function: controls output state of a light
+   Output: returns a boolean to inform user of lights current state"""
+def Light(pin, light):
+    initalizeOut(pin)
 
     #Handling of day Ligh
-    Light_SP = int((float(Light)/(100.00))*(24.00))
-    Hour = strftime("%H", gmtime())
-    if Hour <= Light_SP:
-        GPIO.output(Pin, GPIO.LOW)
+    light_SP = int((float(light)/(100.00))*(24.00))
+    hour = strftime("%H", gmtime())
+    if hour <= light_SP:
+        GPIO.output(pin, True)
         return True
-    elif Hour > Light_SP:
-        GPIO.output(Pin, GPIO.HIGH)
+    elif hour > light_SP:
+        GPIO.output(pin, False)
         return False
 
-#Power control for pump
-def Pump(P_Pin, WS_Pin):
-    InitalizeOut(P_Pin)
-    InitalizeIn(WS_Pin)
+"""Input: pin - integer value containing the pump pin location
+          ws - integer value containing the current value of the water sensor
+   Function: controls output state of a pump
+   Output: returns a boolean to inform user of pumps current state"""
+def Pump(pin, ws):
+    initalizeOut(pin)
 
     #Handaling of water pumps
-    if GPIO.input(WS_Pin):
-        GPIO.output(P_Pin, GPIO.HIGH)
+    if int(ws) >= 200:
+        GPIO.output(pin, False)
         return False
     else:
-        GPIO.output(P_Pin, GPIO.LOW)
+        GPIO.output(pin, True)
         return True
 
-#Power control for mister
-def Mister(Pin, Humidity, Humidity_SP):
-    InitalizeOut(Pin)
+"""Input: pin - integer value containing the mister pin location
+          humidity - integer value containing the current humidity value
+          humidity_sp - integer value containing the wanted humidity value
+   Function: controls output state of a mister
+   Output: returns a boolean to inform user of mister current state"""
+def Mister(pin, humidity, humidity_sp):
+    initalizeOut(pin)
 
-    if int(Humidity) < int(Humidity_SP):
-        GPIO.output(Pin, True)
+    if int(humidity) < int(humidity_sp):
+        GPIO.output(pin, True)
         return True
     else:
-        GPIO.output(Pin, False)
+        GPIO.output(pin, False)
         return False
 
-#Power control for fan
-def Fan(Pin, Output):
-    InitalizeOut(Pin)
+"""Input: pin - integer value containing the desired pin
+          output - boolean value containing the desired output of the fan
+   Function: controls output state of a fan
+   Output: returns a boolean to inform user of fans current state"""
+def Fan(pin, output):
+    initalizeOut(pin)
 
     #Handling of fan
-    if Output == True:
-        GPIO.output(Pin, GPIO.LOW)
+    if output == True:
+        GPIO.output(pin, True)
         return True
-    elif Output == False:
-        GPIO.output(Pin, GPIO.HIGH)
+    elif output == False:
+        GPIO.output(pin, False)
         return False
 
-#Power control for hot plate
-def HotPlate(Pin, Output):
-    InitalizeOut(Pin)
+"""Input: pin - integer value containing the desired pin
+          output - boolean value containing the desired output of the fan
+   Function: controls output state of a hotplate
+   Output: returns a boolean to inform user of hotplates current state"""
+def hotPlate(pin, output):
+    initalizeOut(Pin)
 
     #Handling of hot plate
-    if Output == True:
-        GPIO.output(Pin, GPIO.LOW)
+    if output == True:
+        GPIO.output(pin, GPIO.LOW)
         return True
-    elif Output == False:
-        GPIO.output(Pin, GPIO.HIGH)
+    elif output == False:
+        GPIO.output(pin, GPIO.HIGH)
         return False
 
 if __name__ == "__main__":
@@ -150,10 +193,10 @@ if __name__ == "__main__":
     Mister_Pin = 25 #Mister GPIO 6
 
     #Inputs
-    T1 = sensor_value("T1","C")
-    H1 = sensor_value("H1","%")
-    WL1 = sensor_value("WL1","")
-    F1 = sensor_value("F1","")
+    T1 = sensor_Value("T1","C")
+    H1 = sensor_Value("H1","%")
+    WL1 = sensor_Value("WL1","")
+    F1 = sensor_Value("F1","")
 
     #Processing of inputs
     Temp = T1
@@ -167,4 +210,4 @@ if __name__ == "__main__":
     Mister(Mister_Pin, Humid, Humidity_SP)
 
     #Pump(P0_Pin, WL1)
-    #HotPlate(HP1_Pin, True)
+    #hotPlate(HP1_Pin, True)
