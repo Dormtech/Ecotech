08f1055fe7119f5d34b98e483f2427100e1120fb
skalapos <steven.kalapos@gmail.com>
2018-05-22 19:50:35 -0400

    attempted to ad default screen
    more research on kivy needed, added website on data driven GUI in kivy

--- RaspberryPi/src/mainGUI.py
+++ RaspberryPi/src/mainGUI.py
@@ -12,10 +12,33 @@ import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.graphics import Color, Rectangle
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.properties import StringProperty
+from kivy.uix.widget import Widget
+from kivy.clock import Clock
 
+
+import random
+
+##https://github.com/kivy/kivy/wiki/Data-driven-variables-with-kivy-properties##
+
+#default "screen saver"
+class defaultDisplay(Widget):
+    
+
+    def __init__(self, **kwargs):
+        super(defaultDisplay, self).__init__(**kwargs)
+        event = Clock.schedule_interval(self.getTemp,1/1.)
+        temperatureVar = StringProperty('')
+        self.getTemp()
+        self.add_widget()
+    
+    def getTemp(self):
+        temperatureVar = str(random.randint(1,10))
+
+#main App GUI control
 class ecozone(App):
 
     def build(self):
-        return Label(text='Hello world')
+        return defaultDisplay()
