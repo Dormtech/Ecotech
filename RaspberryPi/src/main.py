10de3e156459b8439f0a085d2bf17ce50b447ee6
skalapos <steven.kalapos@gmail.com>
2018-05-22 14:34:26 -0400

    reorganize and add GUI stuffs

--- RaspberryPi/src/main.py
+++ RaspberryPi/src/main.py
@@ -1,11 +1,12 @@
 """
  * @file main.py
  * @authors Steven Kalapos & Ben Bellerose
- * @date May 2018
- * @modified May 21 2018
- * @modifiedby Ben Bellerose
+ * @date May 21 2018
+ * @modified May 22 2018
+ * @modifiedby SK
  * @brief main control file for device
  */
  """
+import kivy
 
 if __name__ == "__main__":
---
+++ RaspberryPi/src/mainGUI.py
@@ -0,0 +1,9 @@
+"""
+ * @file mainGUI.py
+ * @authors Steven Kalapos & Ben Bellerose
+ * @date May 22 2018
+ * @modified May 22 2018
+ * @modifiedby SK
+ * @brief GUI managment and creation
+ */
+ """
