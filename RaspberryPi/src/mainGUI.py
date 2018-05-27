b4b388d38919600c222ccf516e197edca02b6245
skalapos <steven.kalapos@gmail.com>
2018-05-27 18:36:35 -0400

    var update figured out
    got var updates in kivy working

--- RaspberryPi/src/mainGUI.py
+++ RaspberryPi/src/mainGUI.py
@@ -2,7 +2,7 @@
  * @file mainGUI.py
  * @authors Steven Kalapos & Ben Bellerose
  * @date May 22 2018
- * @modified May 25 2018
+ * @modified May 27 2018
  * @modifiedby SK
  * @brief GUI managment and creation
  */
@@ -14,7 +14,7 @@ from kivy.uix.label import Label
 from kivy.graphics import Color, Rectangle
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
-from kivy.properties import StringProperty, NumericProperty, Property 
+from kivy.properties import StringProperty, NumericProperty, ObjectProperty 
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -22,21 +22,24 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 
 import random
 
-
 ##https://github.com/kivy/kivy/wiki/Data-driven-variables-with-kivy-properties##
 
 #default "screen saver"
 class defaultScreenLayout(GridLayout):
-    
-    temperatureK = "8"
 
     def __init__(self, **kwargs):
-        super(defaultScreenLayout, self).__init__(**kwargs)
+        super(defaultScreenLayout, self).__init__(**kwargs)   
 
-    def updateTemp(self):
-        self.temperatureK = str(random.randint(1,100))
+        self.temperatureVar = Label()
+        self.add_widget(self.temperatureVar)
+        Clock.schedule_interval(self.update, 1)
+        
+    def update(self, dt):
+        self.temperatureVar.text = str(random.randint(0,200))
+
     
-    Clock.schedule_interval(updateTemp,2)
+    
+
 
 #main App GUI control
 class ecozoneApp(App):
