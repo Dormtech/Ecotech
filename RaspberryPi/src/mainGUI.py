e90d4fbdc01c3c722a916f027544e2a1c6485f31
skalapos <steven.kalapos@gmail.com>
2018-05-31 14:10:23 -0400

    missed a colon

--- RaspberryPi/src/mainGUI.py
+++ RaspberryPi/src/mainGUI.py
@@ -24,7 +24,7 @@ import random, os
 
 class GUIFunc():
     #creates the option file used to store user config
-    def createGUIOptions(pathway)
+    def createGUIOptions(pathway):
         optionFile = open(pathway+"/options/GUIOptions.txt","w+")
         #write some more stuff to file
 
