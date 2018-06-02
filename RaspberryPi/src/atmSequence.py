9c43cc37e9e059f1c354deb87155186d71421bf4
Ben <belleroseb@hotmail.com>
2018-06-02 14:59:10 -0400

    Updated atmSequence
    updated main control of atmsequence

--- RaspberryPi/src/atmSequence.py
+++ RaspberryPi/src/atmSequence.py
@@ -2,7 +2,7 @@
  * @file atmSequence.py
  * @authors Steven Kalapos & Ben Bellerose
  * @date May 2018
- * @modified May 30 2018
+ * @modified June 2 2018
  * @modifiedby BB
  * @brief control sequence for the atmosphere of the machine
  */
@@ -16,7 +16,7 @@ class atmosphere():
               tempatureSP - integer value containing the tempature setpoint in degrees celsius
               light - integer value 0-100 describing the percentage of ligth needed for the day
         Function: controls the atmosphere inside the machine
-        Output: returns a boolean value to inform user of machine state"""
+        Output: returns a boolean value to inform the user if the function was compleated or not"""
     def atmMain(self,humiditySP,carbonSP,tempatureSP,light):
         if humiditySP is not None:
             if carbonSP is not None:
@@ -27,8 +27,10 @@ class atmosphere():
                             L1_Pin = 26 #Light GPIO 25
                             F1_Pin = 6 #Circulation fan GPIO 22
                             F2_Pin = 8 #Exhaust fan GPIO 10
-                            F3_Pin = 8 #Electrical exhaust fan GPIO 10
-                            F4_Pin = 8 #Electrical intake fan GPIO 10
+                            F3_Pin = 8 #Intake fan GPIO 10
+                            F4_Pin = 8 #Transition fan GPIO 10
+                            F5_Pin = 8 #Electrical exhaust fan GPIO 10
+                            F6_Pin = 8 #Electrical intake fan GPIO 10
                             M1_Pin = 25 #Mister GPIO 6
 
                             #Inputs
@@ -39,33 +41,34 @@ class atmosphere():
                             if run_mode().sensor_Value("F1","") == "ERROR":
                                 f1 = 0
                             else:
-                                f1 = int(run_mode().sensor_Value("F1",""))
+                                f1 = run_mode().sensor_Value("F1","")
 
                             if run_mode().sensor_Value("F2","") == "ERROR":
                                 f2 = 0
                             else:
-                                f2 = int(run_mode().sensor_Value("F2",""))
+                                f2 = run_mode().sensor_Value("F2","")
 
                             if run_mode().sensor_Value("F3","") == "ERROR":
                                 f3 = 0
                             else:
-                                f3 = int(run_mode().sensor_Value("F3",""))
+                                f3 = run_mode().sensor_Value("F3","")
 
                             if run_mode().sensor_Value("F4","") == "ERROR":
                                 f4 = 0
                             else:
-                                f4 = int(run_mode().sensor_Value("F4",""))
+                                f4 = run_mode().sensor_Value("F4","")
 
                             if run_mode().sensor_Value("F5","") == "ERROR":
                                 f5 = 0
                             else:
-                                f5 = int(run_mode().sensor_Value("F5",""))
+                                f5 = run_mode().sensor_Value("F5","")
 
                             #Processing of inputs
-                            temp = t1
-                            humid = h1
-                            carbon = c1
-                            fire = f1 + f2 + f3 + f4 + f5
+                            temp = int(t1)
+                            humid = int(h1)
+                            elecTemp = int(t1)
+                            carbon = int(c1)
+                            fire = int(f1) + int(f2) + int(f3) + int(f4) + int(f5)
 
                             #Output control
                             fireLevel = 10
@@ -73,12 +76,64 @@ class atmosphere():
                                 deviceControl().Light(L1_Pin,light) #Light
                                 deviceControl().Fan(F1_Pin, True) #Circulation
 
-                                if tempatureSP <= temp:
-                                    deviceControl().Fan(F2_Pin, True) #Exhaust
+                                if 40 <= elecTemp: #Elctrical box to hot
+                                    if tempatureSP <= temp: #Too hot
+                                        if carbonSP <= carbon: #Too much carbon dioxide
+                                            deviceControl().Fan(F2_Pin, True) #Exhaust
+                                            deviceControl().Fan(F3_Pin, True) #Intake
+                                            deviceControl().Fan(F4_Pin, False) #Transition
+                                            deviceControl().Fan(F5_Pin, True) #Electrical exhaust
+                                            deviceControl().Fan(F6_Pin, True) #Electrical intake
+                                        else:
+                                            deviceControl().Fan(F2_Pin, False) #Exhaust
+                                            deviceControl().Fan(F3_Pin, True) #Intake
+                                            deviceControl().Fan(F4_Pin, True) #Transition
+                                            deviceControl().Fan(F5_Pin, False) #Electrical exhaust
+                                            deviceControl().Fan(F6_Pin, True) #Electrical intake
+                                    else:
+                                        if carbonSP <= carbon: #Too much carbon dioxide
+                                            deviceControl().Fan(F2_Pin, False) #Exhaust
+                                            deviceControl().Fan(F3_Pin, False) #Intake
+                                            deviceControl().Fan(F4_Pin, False) #Transition
+                                            deviceControl().Fan(F5_Pin, True) #Electrical exhaust
+                                            deviceControl().Fan(F6_Pin, True) #Electrical intake
+                                        else:
+                                            deviceControl().Fan(F2_Pin, False) #Exhaust
+                                            deviceControl().Fan(F3_Pin, False) #Intake
+                                            deviceControl().Fan(F4_Pin, True) #Transition
+                                            deviceControl().Fan(F5_Pin, False) #Electrical exhaust
+                                            deviceControl().Fan(F6_Pin, True) #Electrical intake
                                 else:
-                                    deviceControl().Fan(F2_Pin, False) #Exhaust
+                                    if tempatureSP <= temp: #Too hot
+                                        if carbonSP <= carbon: #Too much carbon dioxide
+                                            deviceControl().Fan(F2_Pin, True) #Exhaust
+                                            deviceControl().Fan(F3_Pin, True) #Intake
+                                            deviceControl().Fan(F4_Pin, False) #Transition
+                                            deviceControl().Fan(F5_Pin, False) #Electrical exhaust
+                                            deviceControl().Fan(F6_Pin, False) #Electrical intake
+                                        else:
+                                            deviceControl().Fan(F2_Pin, False) #Exhaust
+                                            deviceControl().Fan(F3_Pin, False) #Intake
+                                            deviceControl().Fan(F4_Pin, True) #Transition
+                                            deviceControl().Fan(F5_Pin, False) #Electrical exhaust
+                                            deviceControl().Fan(F6_Pin, True) #Electrical intake
+                                    else:
+                                        if carbonSP <= carbon: #Too much carbon dioxide
+                                            deviceControl().Fan(F2_Pin, False) #Exhaust
+                                            deviceControl().Fan(F3_Pin, False) #Intake
+                                            deviceControl().Fan(F4_Pin, False) #Transition
+                                            deviceControl().Fan(F5_Pin, False) #Electrical exhaust
+                                            deviceControl().Fan(F6_Pin, False) #Electrical intake
+                                        else:
+                                            deviceControl().Fan(F2_Pin, False) #Exhaust
+                                            deviceControl().Fan(F3_Pin, True) #Intake
+                                            deviceControl().Fan(F4_Pin, True) #Transition
+                                            deviceControl().Fan(F5_Pin, False) #Electrical exhaust
+                                            deviceControl().Fan(F6_Pin, True) #Electrical intake
 
                                 deviceControl().Mister(M1_Pin, humid, humiditySP) #Mister
+                                return True
+                                
                             else:
                                 if f1 > fireLevel:
                                     deviceControl().Fire("F1")
@@ -90,8 +145,7 @@ class atmosphere():
                                     deviceControl().Fire("F4")
                                 elif f5> fireLevel:
                                     deviceControl().Fire("F5")
-                            return True
-                            
+                                return False
                         except Exception as e:
                             errCode = "ERROR COUNTROLING ATMOSPHERE"
                             errMsg = "Error controling atmosphere. The following error code appeared; " + e + "."
