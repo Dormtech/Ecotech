75170e91fbdb6dea1ab34f6d337eb83a0f5b8b2e
Ben <belleroseb@hotmail.com>
2018-06-25 11:23:49 -0400

    New test

--- RaspberryPi/src/test.py
+++ RaspberryPi/src/test.py
@@ -2,15 +2,20 @@
  * @file test.py
  * @authors Steven Kalapos & Ben Bellerose
  * @date May 26 2018
- * @modified May 26 2018
+ * @modified June 25 2018
  * @modifiedby BB
  * @brief testing playground to try out classes
  */
  """
+ from atmSequence import atmosphere
+ from pumpSequence import pumps
 
 if __name__ == "__main__":
-    num = 4
-    if type(num) is int:
-        print("YEP")
-    else:
-        print("NOPE")
+    humiditySP = 60
+    carbonSP = 10
+    tempatureSP = 30
+    light = 80
+    elecSP = 40
+    phSP = 50
+    atmosphere().atmMain(humiditySP,carbonSP,tempatureSP,light,elecSP)
+    pumps().pumpMain(phSP)
