57a399aac86f94dfe8691f807f5b3358e109d34c
Ben <belleroseb@hotmail.com>
2018-07-15 11:34:38 -0400

    Added timer

--- RaspberryPi/src/test.py
+++ RaspberryPi/src/test.py
@@ -7,11 +7,13 @@
  * @brief testing playground to try out classes
  */
  """
+import time
 from atmSequence import atmosphere
 from pumpSequence import pumps
 from networking import network
 
 if __name__ == "__main__":
+    startTime = time.time()
     humiditySP = 60
     carbonSP = 10
     tempatureSP = 30
@@ -20,8 +22,16 @@ if __name__ == "__main__":
     phSP = 50
     #network().machineUpdate()
     x = 0
-    while x < 100:
+    while x < 1:
+        print("Count = " + str(x + 1))
         atmosphere().atmMain(humiditySP,carbonSP,tempatureSP,light,elecSP)
+        #pumps().pumpMain(phSP)
+        print("")
         x = x + 1
-        print("Count = " + str(x))
-    #pumps().pumpMain(phSP)
+    endTime = time.time()
+    elapsedTime = endTime - startTime
+    print("**********************************")
+    print("Start time: " + time.strftime("%H:%M:%S", time.gmtime(start)))
+    print("End time: " + time.strftime("%H:%M:%S", time.gmtime(end)))
+    print("Elapsed time: " + time.strftime("%H:%M:%S", time.gmtime(start)))
+    print("**********************************")
