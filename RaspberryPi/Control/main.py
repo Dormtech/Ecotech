f3b709b17c6bd311dc43fc52b001df2106e1d888
Ben <belleroseb@hotmail.com>
2018-07-23 11:39:11 -0400

    Removed Cheech

--- RaspberryPi/Control/main.py
+++
@@ -1,7 +0,0 @@
-#*********************************************************************
-#main.py
-#Authors:Jake Smiley & Ben Bellerose & Steven Kalapos
-#Description: Main control for Ecozone
-#*********************************************************************
-
-if __name__ == "__main__":
--- RaspberryPi/Control/run.py
+++
@@ -1,5 +0,0 @@
-#*********************************************************************
-#Cheech V_01
-#Authors:Jake Smiley & Ben Bellerose & Steven Kalapos
-#Description: Main control for Ecozone
-#*********************************************************************
