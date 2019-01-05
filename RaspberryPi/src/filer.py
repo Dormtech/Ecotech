7363cbabeefc0f82127d81b65ebccbe8d2375c09
Ben <belleroseb@hotmail.com>
2019-01-05 16:39:41 -0500

    Formating

---
+++ RaspberryPi/src/filer.py
@@ -0,0 +1,11 @@
+"""
+ * @file filer.py
+ * @authors Steven Kalapos & Ben Bellerose
+ * @date January 5 2019
+ * @modified January 5 2019
+ * @modifiedby BB
+ * @brief class dedicated to file management within the machine
+ */
+ """
+
+class filer():
